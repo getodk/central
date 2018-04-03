234b8357995c100c6c177293d52bd11c7296d8d7
Clint Tseng <clint@dontexplain.com>
2018-04-03 15:01:46 -0700

    improve: change service cron invocation so it is owned by bash.

--- files/service/scripts/start-odk.sh
+++ files/service/scripts/start-odk.sh
@@ -9,7 +9,7 @@ echo "running migrations.."
 node -e 'const { withDatabase, migrate } = require("./lib/model/database"); withDatabase(require("config").get("default.database"))(migrate);'
 
 echo "starting cron.."
-cron
+cron -f &
 
 echo "starting server."
 node lib/bin/run-server.js
