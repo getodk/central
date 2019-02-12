3f043380a22d3b71e41fb1d48d0bac425f81aed8
Clint Tseng <clint@dontexplain.com>
2019-02-11 16:23:47 -0800

    new: use ngx_lua to decompress gzipped request bodies.

---
+++ files/nginx/inflate_body.lua
@@ -0,0 +1,63 @@
+-- from http://www.pataliebre.net/howto-make-nginx-decompress-a-gzipped-request.html
+
+ngx.ctx.max_chunk_size = tonumber(ngx.var.max_chunk_size)
+ngx.ctx.max_body_size = tonumber(ngx.var.max_body_size)
+
+function create_error_response (code, description)
+  local message = string.format('{"status":400,"statusReason":"Bad Request","code":%d,"exception":"","description":"%s","message":"HTTP 400 Bad Request"}', code, description)
+  ngx.status = ngx.HTTP_BAD_REQUEST
+  ngx.header.content_type = "application/json"
+  ngx.say(message)
+  ngx.exit(ngx.HTTP_OK)
+end
+
+function inflate_chunk (stream, chunk)
+  return stream(chunk)
+end
+
+function inflate_body (data)
+  local stream = require("zlib").inflate()
+  local buffer = ""
+  local chunk = ""
+
+  for index = 0, data:len(), ngx.ctx.max_chunk_size do
+    chunk = string.sub(data, index, index + ngx.ctx.max_chunk_size - 1)
+    local status, output, eof, bytes_in, bytes_out = pcall(stream, chunk)
+
+    if not status then
+      -- corrupted chunk
+      ngx.log(ngx.ERR, output)
+      create_error_response(4001, "Corrupted GZIP body")
+    end
+
+    if bytes_in == 0 and bytes_out == 0 then
+      -- body is not gzip compressed
+      create_error_response(4002, "Invalid GZIP body")
+    end
+
+    buffer = buffer .. output
+
+    if bytes_out > ngx.ctx.max_body_size then
+      -- uncompressed body too large
+      create_error_response(4003, "Uncompressed body too large")
+    end
+  end
+
+  return buffer
+end
+
+
+local content_encoding = ngx.req.get_headers()["Content-Encoding"]
+if content_encoding == "gzip" then
+  ngx.req.read_body()
+  local data = ngx.req.get_body_data()
+
+  if data ~= nil and data ~= '' then
+    local new_data = inflate_body(data)
+
+    ngx.req.clear_header("Content-Encoding")
+    ngx.req.clear_header("Content-Length")
+    ngx.req.set_body_data(new_data)
+  end
+end
+
--- nginx.dockerfile
+++ nginx.dockerfile
@@ -13,7 +13,7 @@ EXPOSE 443
 VOLUME [ "/etc/dh", "/etc/selfsign", "/etc/nginx/conf.d" ]
 ENTRYPOINT [ "/bin/bash", "/scripts/odk-setup.sh" ]
 
-RUN apt-get update; apt-get install -y openssl netcat
+RUN apt-get update; apt-get install -y openssl netcat nginx-extras lua-zlib
 
 RUN mkdir -p /etc/selfsign/live/local
 COPY files/nginx/odk-setup.sh /scripts
@@ -21,6 +21,7 @@ COPY files/nginx/odk-setup.sh /scripts
 COPY files/local/customssl/*.pem /etc/customssl/live/local/
 
 COPY files/nginx/odk.conf.template /usr/share/nginx
+COPY files/nginx/inflate_body.lua /usr/share/nginx
 COPY --from=intermediate client/ /usr/share/nginx/html
 COPY --from=intermediate /tmp/version.txt /usr/share/nginx/html/
 
