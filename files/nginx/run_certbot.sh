74f3a4f48525410827e033d882c0e890b985df1c
Clint Tseng <clint@dontexplain.com>
2019-02-22 15:20:05 -0800

    bug: sending SIGHUP too quickly fails nginx out.
    * i don't know why.

---
+++ files/nginx/run_certbot.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Source in util.sh so we can have our nice tools
+. $(cd $(dirname $0); pwd)/util.sh
+
+# We require an email to register the ssl certificate for
+if [ -z "$CERTBOT_EMAIL" ]; then
+    error "CERTBOT_EMAIL environment variable undefined; certbot will do nothing"
+    exit 1
+fi
+
+exit_code=0
+set -x
+# Loop over every domain we can find
+for domain in $(parse_domains); do
+    if is_renewal_required $domain; then
+        # Renewal required for this doman.
+        # Last one happened over a week ago (or never)
+        if ! get_certificate $domain $CERTBOT_EMAIL; then
+            error "Cerbot failed for $domain. Check the logs for details."
+            exit_code=1
+        fi
+    else
+        echo "Not run certbot for $domain; last renewal happened just recently."
+    fi
+done
+
+# After trying to get all our certificates, auto enable any configs that we
+# did indeed get certificates for
+auto_enable_configs
+
+# Finally, tell nginx to reload the configs
+sleep 5 # if we SIGHUP too soon nginx fails for some reason.
+kill -HUP $NGINX_PID
+
+set +x
+exit $exit_code
