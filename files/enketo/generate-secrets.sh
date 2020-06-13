52248f848c5599d997ba8464dda1584b60223740
Issa Tseng <maintainer@heyissa.com>
2020-06-12 20:05:06 -0700

    enketo: incorporate enketo into the docker setup.

---
+++ files/enketo/generate-secrets.sh
@@ -0,0 +1,13 @@
+
+if [ ! -f /etc/secrets/enketo-secret ]; then
+  LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c64 > /etc/secrets/enketo-secret
+fi
+
+if [ ! -f /etc/secrets/enketo-less-secret ]; then
+  LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c32 > /etc/secrets/enketo-less-secret
+fi
+
+if [ ! -f /etc/secrets/enketo-api-key ]; then
+  LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c128 > /etc/secrets/enketo-api-key
+fi
+
