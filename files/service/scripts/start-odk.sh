82075b7fddd8961fff27ff14ce2b23e969329fae
Matthew White <matthew-white@users.noreply.github.com>
2021-04-14 10:45:46 -0400

    Update path to migrations after Backend refactor
    Related to getodk/central-backend#331.

--- files/service/scripts/start-odk.sh
+++ files/service/scripts/start-odk.sh
@@ -3,7 +3,7 @@ echo "generating local service configuration.."
 /bin/bash -c "ENKETO_API_KEY=$(cat /etc/secrets/enketo-api-key) envsubst '\$DOMAIN:\$SYSADMIN_EMAIL:\$ENKETO_API_KEY' < /usr/share/odk/config.json.template > $CONFIG_PATH"
 
 echo "running migrations.."
-node -e 'const { withDatabase, migrate } = require("./lib/model/database"); withDatabase(require("config").get("default.database"))(migrate);'
+node -e 'const { withDatabase, migrate } = require("./lib/model/migrate"); withDatabase(require("config").get("default.database"))(migrate);'
 
 echo "starting cron.."
 cron -f &
