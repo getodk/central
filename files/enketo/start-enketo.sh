b05790231732c38ae7495787dd3b65a259b27357
alxndrsn <alxndrsn>
2022-10-07 10:10:59 +0300

    Add shebangs to all shell scripts

--- files/enketo/start-enketo.sh
+++ files/enketo/start-enketo.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 CONFIG_PATH=${ENKETO_SRC_DIR}/config/config.json
 echo "generating enketo configuration.."
 /bin/bash -c "SECRET=$(cat /etc/secrets/enketo-secret) LESS_SECRET=$(cat /etc/secrets/enketo-less-secret) API_KEY=$(cat /etc/secrets/enketo-api-key) envsubst '\$DOMAIN\$HTTPS_PORT:\$SECRET:\$LESS_SECRET:\$API_KEY:\$SUPPORT_EMAIL' < ${CONFIG_PATH}.template > $CONFIG_PATH"
