9974c4a4c5f834b7a150d7f96016ea0fbc3b6a02
Hélène Martin <lognaturel@gmail.com>
2023-03-08 11:20:35 -0800

    Only include HTTPS port in base URL if it's not 443

--- files/enketo/start-enketo.sh
+++ files/enketo/start-enketo.sh
@@ -3,13 +3,13 @@
 CONFIG_PATH=${ENKETO_SRC_DIR}/config/config.json
 echo "generating enketo configuration.."
 
+BASE_URL=$( [ "${HTTPS_PORT}" = 443 ] && echo https://"${DOMAIN}" || echo https://"${DOMAIN}":"${HTTPS_PORT}" ) \
 SECRET=$(cat /etc/secrets/enketo-secret) \
 LESS_SECRET=$(cat /etc/secrets/enketo-less-secret) \
 API_KEY=$(cat /etc/secrets/enketo-api-key) \
-envsubst '$DOMAIN,$HTTPS_PORT,$SECRET,$LESS_SECRET,$API_KEY,$SUPPORT_EMAIL' \
+envsubst '$DOMAIN,$BASE_URL,$SECRET,$LESS_SECRET,$API_KEY,$SUPPORT_EMAIL' \
     < "$CONFIG_PATH.template" \
     > "$CONFIG_PATH"
 
 echo "starting pm2/enketo.."
 pm2 start --no-daemon app.js -n enketo
-
