59ec8538e023af4def07cc5f946e8de98749bb6f
Yaw Anokwa <yanokwa@users.noreply.github.com>
2023-03-08 16:17:20 -0800

    Revert named volumes because certs can be transient (#404)

--- files/nginx/setup-odk.sh
+++ files/nginx/setup-odk.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-NGINX_PATH=/etc/odk/nginx
-
-DH_PATH="$NGINX_PATH/ssl_dhparam.pem"
+DH_PATH=/etc/dh/nginx.pem
 if [ "$SSL_TYPE" != "upstream" ] && [ ! -s "$DH_PATH" ]; then
   openssl dhparam -out "$DH_PATH" 2048
 fi
 
-SELFSIGN_PATH="$NGINX_PATH/selfsign"
+SELFSIGN_PATH="/etc/selfsign/live/$DOMAIN"
 if [ "$SSL_TYPE" = "selfsign" ] && [ ! -s "$SELFSIGN_PATH/privkey.pem" ]; then
   mkdir -p "$SELFSIGN_PATH"
   openssl req -x509 -newkey rsa:4086 \
@@ -19,28 +17,28 @@ fi
 
 # start from fresh templates in case ssl type has changed
 echo "writing fresh nginx templates..."
-cp /usr/share/odk/nginx/conf/redirector.conf /etc/nginx/conf.d/redirector.conf
-CNAME=$({ [ "$SSL_TYPE" = "customssl" ] || [ "$SSL_TYPE" = "selfsign" ]; } && echo "local" || echo "$DOMAIN") \
-envsubst '$CNAME' \
-  < /usr/share/odk/nginx/conf/odk.conf.template \
+cp /usr/share/odk/nginx/redirector.conf /etc/nginx/conf.d/redirector.conf
+CNAME=$( [ "$SSL_TYPE" = "customssl" ] && echo "local" || echo "$DOMAIN") \
+envsubst '$SSL_TYPE $CNAME' \
+  < /usr/share/odk/nginx/odk.conf.template \
   > /etc/nginx/conf.d/odk.conf
 
 if [ "$SSL_TYPE" = "letsencrypt" ]; then
-  echo "starting nginx with certbot..."
+  echo "starting nginx for letsencrypt..."
   /bin/bash /scripts/start_nginx_certbot.sh
-elif [ "$SSL_TYPE" = "customssl" ] || [ "$SSL_TYPE" = "selfsign" ]; then
-  CERT_PATH=$( [ "$SSL_TYPE" = "customssl" ] && echo "/usr/share/odk/nginx/customssl" || echo "$SELFSIGN_PATH") 
-  # replace letsencrypt cert locations with customssl/selfsign location
-  perl -i -pe "s|/etc/letsencrypt/live/local|$CERT_PATH|;" /etc/nginx/conf.d/odk.conf
-  # strip out HTTP-01 ACME challenge location
-  perl -i -ne 'print if $. < 7 || $. > 14' /etc/nginx/conf.d/redirector.conf
-  echo "starting nginx without certbot..."
-  nginx -g "daemon off;"
-elif [ "$SSL_TYPE" = "upstream" ]; then
-  # strip out all ssl_* directives
-  perl -i -ne 's/listen 443.*/listen 80;/; print if ! /ssl_/' /etc/nginx/conf.d/odk.conf
-  # force https because we expect SSL upstream
-  perl -i -pe 's/X-Forwarded-Proto \$scheme/X-Forwarded-Proto https/;' /etc/nginx/conf.d/odk.conf
-  echo "starting nginx without local SSL to allow for upstream SSL..."
+else
+  if [ "$SSL_TYPE" = "upstream" ]; then
+    # no need for letsencrypt challenge reply or 80 to 443 redirection
+    rm -f /etc/nginx/conf.d/redirector.conf
+    # strip out all ssl_* directives
+    perl -i -ne 's/listen 443.*/listen 80;/; print if ! /ssl_/' /etc/nginx/conf.d/odk.conf
+    # force https because we expect SSL upstream
+    perl -i -pe 's/X-Forwarded-Proto \$scheme/X-Forwarded-Proto https/;' /etc/nginx/conf.d/odk.conf
+    echo "starting nginx for upstream ssl..."
+  else
+    # remove letsencrypt challenge reply, but keep 80 to 443 redirection
+    perl -i -ne 'print if $. < 7 || $. > 14' /etc/nginx/conf.d/redirector.conf
+    echo "starting nginx for custom ssl and self-signed certs..."
+  fi
   nginx -g "daemon off;"
 fi
