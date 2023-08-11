735cbe72ad0b974f6905826c026b5733af1bbc17
Matthew White <matthew-white@users.noreply.github.com>
2023-08-10 22:48:00 -0400

    Can we echo OIDC_DISCOVERY_URL if we pass it via environment?

--- files/prebuild/build-frontend.sh
+++ files/prebuild/build-frontend.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eu
+echo "\$OIDC_DISCOVERY_URL in build-frontend.sh: [${OIDC_DISCOVERY_URL:-blank}]"
 cd client
 npm clean-install --no-audit --fund=false --update-notifier=false
-if [[ -n $OIDC_DISCOVERY_URL && -n $OIDC_CLIENT_ID && -n $OIDC_CLIENT_SECRET ]]; then
-  export VUE_APP_OIDC_ENABLED=true
-fi
+# if [[ -n $OIDC_DISCOVERY_URL && -n $OIDC_CLIENT_ID && -n $OIDC_CLIENT_SECRET ]]; then
+#   export VUE_APP_OIDC_ENABLED=true
+# fi
 npm run build
