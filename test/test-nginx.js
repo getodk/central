1a5eda53362a69f47e1e996682d744a9f3837761
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2024-10-07 09:34:45 +0300

    nginx-tests: eliminate duplicate dockerfile (#741)
    pros:
    
    * nginx tests actually run against a real image
    
    cons:
    
    * slows fresh/CI builds by about 1 minute

--- test/test-nginx.js
+++ test/test-nginx.js
@@ -43,22 +43,24 @@ describe('nginx config', () => {
   });
 
   [
-    '/index.html',
-    '/version.txt',
-  ].forEach(staticFile => {
+    [ '/index.html',  /<div id="app"><\/div>/ ],
+    [ '/version.txt', /^versions:/ ],
+  ].forEach(([ staticFile, expectedContent ]) => {
     it(`${staticFile} file should have no-cache header`, async () => {
       // when
       const res = await fetchHttps(staticFile);
 
       // then
       assert.equal(res.status, 200);
-      assert.equal(await res.text(), `hi:${staticFile}\n`);
+      assert.match(await res.text(), expectedContent);
       assert.equal(await res.headers.get('cache-control'), 'no-cache');
     });
   });
 
   [
-    '/should-be-cached.txt',
+    '/blank.html',
+    '/favicon.ico',
+    // there's no way to predict generated asset paths, as they have cache-busting names
   ].forEach(staticFile => {
     it(`${staticFile} file should not have no-cache header`, async () => {
       // when
@@ -66,7 +68,6 @@ describe('nginx config', () => {
 
       // then
       assert.equal(res.status, 200);
-      assert.equal(await res.text(), `hi:${staticFile}\n`);
       assert.isNull(await res.headers.get('cache-control'));
     });
   });
