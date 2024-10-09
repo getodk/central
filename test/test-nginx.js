00b5bd7ecbf5b90b41a14eb4e6ecf035f5651c86
alxndrsn <alxndrsn>
2024-10-09 08:05:40 +0000

    fix test URL

--- test/test-nginx.js
+++ test/test-nginx.js
@@ -128,7 +128,7 @@ describe('nginx config', () => {
 
   it('should reject HTTPS requests with incorrect host header supplied', async () => {
     // when
-    const res = await fetchHttps('/.well-known/acme-challenge', { headers:{ host:'bad.example.com' } });
+    const res = await fetchHttps('/', { headers:{ host:'bad.example.com' } });
 
     // then
     assert.equal(res.status, 421);
