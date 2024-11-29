6338341ea67f8373f3afd23832d5d1a0ad1504a1
Sadiq Khoja <sadiqkhoja@gmail.com>
2024-11-29 15:21:28 -0500

    move enketo dev secrets to docker-compose.dev.yml

--- files/enketo/start-enketo.sh
+++ files/enketo/start-enketo.sh
@@ -6,10 +6,7 @@ echo "generating enketo configuration..."
 if [ "$ENV" = "DEV" ]; then
     sed -i -e 's/enketo_redis_main/localhost/g' \
        -e 's/enketo_redis_cache/localhost/g' \
-       -e 's/6380/6379/g' \
-       -e 's/${SECRET}/s0m3v3rys3cr3tk3y/g' \
-       -e 's/${LESS_SECRET}/this $3cr3t key is crackable/g' \
-       -e 's/${API_KEY}/enketorules/g' "$CONFIG_PATH.template"
+       -e 's/6380/6379/g' "$CONFIG_PATH.template"
 fi
 
 BASE_URL=$( [ "${HTTPS_PORT}" = 443 ] && echo https://"${DOMAIN}" || echo https://"${DOMAIN}":"${HTTPS_PORT}" ) \
