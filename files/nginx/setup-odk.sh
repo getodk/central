18dad03788a1b22196b8bc7e1622f5a0c60adeff
alxndrsn <alxndrsn>
2024-12-02 12:08:38 +0000

    untee

--- files/nginx/setup-odk.sh
+++ files/nginx/setup-odk.sh
@@ -37,13 +37,13 @@ fi
 echo "writing fresh nginx templates..."
 # redirector.conf gets deleted if using upstream SSL so copy it back
 envsubst '$DOMAIN' \
-  <   /usr/share/odk/nginx/redirector.conf |
-  tee /etc/nginx/conf.d/redirector.conf
+  < /usr/share/odk/nginx/redirector.conf \
+  > /etc/nginx/conf.d/redirector.conf
 
 CNAME=$( [ "$SSL_TYPE" = "customssl" ] && echo "local" || echo "$DOMAIN") \
 envsubst '$SSL_TYPE $DOMAIN $CNAME $SENTRY_ORG_SUBDOMAIN $SENTRY_KEY $SENTRY_PROJECT' \
-  <   /usr/share/odk/nginx/odk.conf.template |
-  tee /etc/nginx/conf.d/odk.conf
+  < /usr/share/odk/nginx/odk.conf.template \
+  > /etc/nginx/conf.d/odk.conf
 
 if [ "$SSL_TYPE" = "letsencrypt" ]; then
   echo "starting nginx for letsencrypt..."
