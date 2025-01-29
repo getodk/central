fc12898b48225af871c0a22dea85d25724f9a8ef
alxndrsn <alxndrsn>
2025-01-29 07:32:16 +0000

    fix shellcheck?

--- test/test-images.sh
+++ test/test-images.sh
@@ -15,13 +15,13 @@ docker compose build
 # nginx setup could take several minutes due to key generation.
 log "Verifying frontend and backend load..."
 docker compose up -d
-CONTAINER_NAME=$(docker inspect -f '{{.Name}}' $(docker compose ps -q nginx) | cut -c2-)
-docker run --network container:$CONTAINER_NAME \
+CONTAINER_NAME=$(docker inspect -f '{{.Name}}' "$(docker compose ps -q nginx)" | cut -c2-)
+docker run --network container:"$CONTAINER_NAME" \
     appropriate/curl -4 --insecure --retry 30 --retry-delay 10 --retry-connrefused https://localhost/ -H 'Host: local' \
 | tee /dev/tty \
 | grep -q 'ODK Central'
 
-docker run --network container:$CONTAINER_NAME \
+docker run --network container:"$CONTAINER_NAME" \
     appropriate/curl -4 --insecure --retry 20 --retry-delay 2 --retry-connrefused https://localhost/v1/projects -H 'Host: local' \
 | tee /dev/tty \
 | grep -q '\[\]'
@@ -29,7 +29,7 @@ docker run --network container:$CONTAINER_NAME \
 log "Verifying pm2..."
 docker compose exec -T service npx pm2 list \
 | tee /dev/tty \
-| grep -c "online"
+| grep -c "online" \
 | grep -q 4
 
 log "All OK."
