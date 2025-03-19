aa23f68f9192cbae50079a4a0470386b5c23b958
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2025-03-19 14:14:51 +0300

    scripts: standardise shebangs (#891)
    * use bash everywhere, instead of a mixture of `sh`, `bash`, and (implicitly) `dash`
    * for consistency, always set flags/options:
      * `-e`: "Exit immediately if a pipeline...returns a non-zero status."
      * `-u`: "Treat unset variables...as an error"
      * `set -o pipefail`: "the return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status"
      * `shopt -s inherit_errexit`: "command substitution inherits the value of the errexit [`-e`] option"
    
    See: https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#The-Set-Builtin
    See: https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#The-Shopt-Builtin
    
    In the service container's `start-odk.sh` script, this also removes the need for the explicit `make check-migrations` check, as all steps in scripts are now checked implicitly.
    
    Closes #341
    Closes #342

--- test/check-scripts.sh
+++ test/check-scripts.sh
@@ -4,6 +4,8 @@ shopt -s inherit_errexit
 
 log() { echo >&2 "[$(basename "$0")] $*"; }
 
+expectedShebang=$'#!/bin/bash -eu\nset -o pipefail\nshopt -s inherit_errexit\n'
+
 scriptFiles="$(cat <(git grep -El '^#!.*sh\b') <(git ls-files | grep -E '.sh$') | sort -u)"
 
 for script in $scriptFiles; do
@@ -32,6 +34,36 @@ for script in $scriptFiles; do
     log "    !!! Tab(s) found."
     log "    !!!"
     log "    !!! Please use spaces for indentation."
+    exit 1
+  fi
+  log "    Passed OK."
+
+  log "  Checking shebang..."
+  shebang="$(head -n3 "$script")"
+  if ! diff <(echo "$shebang") <(printf '%s' "$expectedShebang"); then
+    log "    !!!"
+    log "    !!! Missing or unexpected shebang."
+    log "    !!!"
+    log "    !!! To make reasoning about script behaviour easier, please"
+    log "    !!! use the standard shebang and shell config lines:"
+    log "    !!!"
+    echo
+    printf '%s' "$expectedShebang"
+    echo
+    exit 1
+  fi
+  log "    Passed OK."
+
+  log "  Checking for indirect invocations..."
+  # shellcheck disable=2086
+  if git grep -E "sh.*$(basename "$script")" $scriptFiles; then
+    log "    !!!"
+    log "    !!! Possible indirect invocation(s) found."
+    log "    !!!"
+    log "    !!! Invoking scripts indirectly means they may be run with an"
+    log "    !!! unexpected shell.  Try invoking the script directly and"
+    log "    !!! relying on its shebang."
+    log "    !!!"
     exit 1
   fi
   log "    Passed OK."
