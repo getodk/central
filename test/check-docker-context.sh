497541becc1c448375c587c81baa33596248660d
alxndrsn <alxndrsn>
2025-03-27 06:35:49 +0000

    Add usage message

--- test/check-docker-context.sh
+++ test/check-docker-context.sh
@@ -4,6 +4,15 @@ log() { echo "[$(basename "$0")] $*"; }
 
 # See: https://stackoverflow.com/a/71751097
 
+if [[ $# = 0 ]]; then
+  cat <<EOF
+    USAGE
+      $0 --report
+      $0 [--min-size NUM --max-size NUM | --skip-size] [--min-count NUM --max-count NUM | --skip-count]
+EOF
+  exit 1
+fi
+
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --report) skip_size=true; skip_count=true ;;
