67732bbe009ce8dfb3fe244c9b6a57122e0f4141
alxndrsn <alxndrsn>
2025-04-01 09:41:46 +0000

    run lint direct from CI

--- test/nginx/run-tests.sh
+++ test/nginx/run-tests.sh
@@ -37,6 +37,6 @@ wait_for_http_response  5 localhost:8005/health 200
 log "Waiting for nginx..."
 wait_for_http_response 90 localhost:9000 421
 
-npm run test
+npm run test:nginx
 
 log "Completed OK."
