f315410c12295e76056ef963e220a7171e656603
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2025-04-25 12:14:12 +0300

    Update generated stream size

--- files/enketo/generate-secrets.sh
+++ files/enketo/generate-secrets.sh
@@ -3,13 +3,13 @@ set -o pipefail
 shopt -s inherit_errexit
 
 if [ ! -f /etc/secrets/enketo-secret ]; then
-  head -c1024 /dev/urandom | LC_ALL=C tr -dc '[:alnum:]' | head -c64 > /etc/secrets/enketo-secret
+  head -c1024 /dev/urandom | LC_ALL=C tr -dc '[:alnum:]' | head -c64  > /etc/secrets/enketo-secret
 fi
 
 if [ ! -f /etc/secrets/enketo-less-secret ]; then
-  head -c1024 /dev/urandom | LC_ALL=C tr -dc '[:alnum:]' | head -c32 > /etc/secrets/enketo-less-secret
+  head -c512  /dev/urandom | LC_ALL=C tr -dc '[:alnum:]' | head -c32  > /etc/secrets/enketo-less-secret
 fi
 
 if [ ! -f /etc/secrets/enketo-api-key ]; then
-  head -c1024 /dev/urandom | LC_ALL=C tr -dc '[:alnum:]' | head -c128 > /etc/secrets/enketo-api-key
+  head -c2048 /dev/urandom | LC_ALL=C tr -dc '[:alnum:]' | head -c128 > /etc/secrets/enketo-api-key
 fi
