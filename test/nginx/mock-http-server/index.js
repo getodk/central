b80f662df8c496acc972a758312976bcefcf3faa
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2025-04-30 08:53:13 +0300

    test-nginx: use proper express middleware (#990)
    The withStdLogging() function was behaving as express middleware, while
    introducing a less idiomatic syntax.  This commit reverts access logs to
    operate as normal express middleware.

--- test/nginx/mock-http-server/index.js
+++ test/nginx/mock-http-server/index.js
@@ -7,14 +7,19 @@ const requests = [];
 
 const app = express();
 
-app.get('/health',      withStdLogging((req, res) => res.send('OK')));
-app.get('/request-log', withStdLogging((req, res) => res.json(requests)));
-app.get('/reset',       withStdLogging((req, res) => {
+app.use((req, res, next) => {
+  console.log(new Date(), req.method, req.originalUrl);
+  next();
+});
+
+app.get('/health',      (req, res) => res.send('OK'));
+app.get('/request-log', (req, res) => res.json(requests));
+app.get('/reset',       (req, res) => {
   requests.length = 0;
   res.json('OK');
-}));
+});
 
-app.get('/v1/reflect-headers', withStdLogging((req, res) => res.json(req.headers)));
+app.get('/v1/reflect-headers', (req, res) => res.json(req.headers));
 
 [
   'delete',
@@ -23,18 +28,11 @@ app.get('/v1/reflect-headers', withStdLogging((req, res) => res.json(req.headers
   'post',
   'put',
   // TODO add more methods as required
-].forEach(method => app[method]('/{*splat}', withStdLogging((req, res) => {
+].forEach(method => app[method]('/{*splat}', (req, res) => {
   requests.push({ method:req.method, path:req.originalUrl });
   res.send('OK');
-})));
+}));
 
 app.listen(port, () => {
   log(`Listening on port: ${port}`);
 });
-
-function withStdLogging(fn) {
-  return (req, res) => {
-    console.log(new Date(), req.method, req.originalUrl);
-    return fn(req, res);
-  };
-}
