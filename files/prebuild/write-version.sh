a2745274f65b25394c9afe20dadda5286e34966c
Yaw Anokwa <yanokwa@users.noreply.github.com>
2025-05-20 19:59:53 -0700

    Pass shellcheck

--- files/prebuild/write-version.sh
+++ files/prebuild/write-version.sh
@@ -5,9 +5,8 @@ shopt -s inherit_errexit
 {
   echo "versions:"
   echo "$(git rev-parse HEAD) ($(git describe --tags))"
-  git submodule foreach --quiet '
-    commit=$(git rev-parse HEAD)
-    tag=$(git describe --tags)
-    echo " $commit $path ($tag)"
-  '
+  git submodule foreach --quiet --recursive \
+    "commit=\$(git rev-parse HEAD); \
+     tag=\$(git describe --tags); \
+     printf ' %s %s (%s)\n' \"\$commit\" \"\$path\" \"\$tag\""
 } > /tmp/version.txt
