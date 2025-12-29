ef19de296c36785431d1e188576ecd7f67ad313c
alxndrsn <alxndrsn>
2025-12-29 17:05:18 +0000

    fix gixy

--- test/nginx/gixy.sh
+++ test/nginx/gixy.sh
@@ -8,16 +8,24 @@ docker_compose() {
   docker compose --file nginx.test.docker-compose.yml "$@"
 }
 
-log "Linting nginx config with gixy-ng..."
-# gixy-ng is a maintained fork of gixy: https://github.com/dvershinin/gixy
-# For version updates, see: https://pypi.org/project/gixy-ng/#history
-docker_compose exec nginx bash -euc '
-  apt update
-  apt install -y python3-venv
-  python3 -m venv .venv
-  . .venv/bin/activate
-  pip install gixy-ng==0.2.12
-  gixy -lll
-'
+lint_service() {
+  local service="$1"
+  log "Linting $service config with gixy-ng..."
+  # gixy-ng is a maintained fork of gixy: https://github.com/dvershinin/gixy
+  # For version updates, see: https://pypi.org/project/gixy-ng/#history
+  docker_compose exec "$service" bash -euc '
+    apt update
+    apt install -y python3-venv
+    python3 -m venv .venv
+    . .venv/bin/activate
+    pip install gixy-ng==0.2.12
+    gixy -lll
+  '
+
+  log "Config looks OK in $service."
+}
+
+lint_service nginx-ssl-selfsign
+lint_service nginx-ssl-upstream
 
 log "Completed OK."
