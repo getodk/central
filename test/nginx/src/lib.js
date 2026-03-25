321f447262217b6bac004c1965cf647f5e3bf85e
alxndrsn <alxndrsn>
2026-03-25 11:35:49 +0000

    remove logging

--- test/nginx/src/lib.js
+++ test/nginx/src/lib.js
@@ -15,7 +15,6 @@ module.exports = {
 async function assertSentryReceived(...expectedRequests) {
   const { status, body } = await requestSentryMock({ path:'/event-log' });
   assert.equal(status, 200);
-  console.log(JSON.stringify({ expectedRequests, body:JSON.parse(body) }, null, 2));
   assert.deepEqual(JSON.parse(body), expectedRequests);
 }
 
