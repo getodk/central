484e76d22f6b3627c349b78a6aa86c238ada9841
alxndrsn <alxndrsn>
2026-04-08 16:26:48 +0000

    test/nginx: check for correct mime types
    Assert that Content-Type headers are set as expected on some common file types.

--- test/nginx/src/mocha/nginx.spec.js
+++ test/nginx/src/mocha/nginx.spec.js
@@ -441,10 +441,15 @@ function standardTestSuite({ fetchHttp, fetchHttp6, apiFetch, apiFetch6, forward
   });
 
   [
-    [ '/index.html',  /<div id="app"><\/div>/ ],
-    [ '/version.txt', /^versions:/ ],
-    [ '/favicon.ico', /^\n$/ ],
-  ].forEach(([ path, expectedContent ]) => {
+    [ '/index.html',                 'text/html', /<div id="app"><\/div>/ ],
+    [ '/version.txt',                'text/plain', /^versions:/ ],
+    [ '/android-chrome-192x192.png', 'image/png',    /^\n$/ ],
+    [ '/android-chrome-512x512.png', 'image/png',    /^\n$/ ],
+    [ '/apple-touch-icon.png',       'image/png',    /^\n$/ ],
+    [ '/favicon.ico',                'image/x-icon', /^\n$/ ],
+    [ '/favicon-16x16.png',          'image/png',    /^\n$/ ],
+    [ '/favicon-32x32.png',          'image/png',    /^\n$/ ],
+  ].forEach(([ path, expectedContentType, expectedContent ]) => {
     it(`${path} file should serve expected content`, async () => {
       // when
       const res = await apiFetch(path);
@@ -452,6 +457,7 @@ function standardTestSuite({ fetchHttp, fetchHttp6, apiFetch, apiFetch6, forward
       // then
       assert.equal(res.status, 200);
       assert.match(await res.text(), expectedContent);
+      assert.equal(res.headers.get('Content-Type'), expectedContentType);
       assertSecurityHeaders(res, { csp:'central-frontend' });
     });
   });
