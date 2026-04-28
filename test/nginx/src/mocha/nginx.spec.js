7308664cb8643fecb0b59c1aebbd7ddd1c6643e9
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-04-28 14:03:10 +0300

    update comments for less slashes
    Co-authored-by: Alex Anderson <191496+alxndrsn@users.noreply.github.com>

--- test/nginx/src/mocha/nginx.spec.js
+++ test/nginx/src/mocha/nginx.spec.js
@@ -54,7 +54,7 @@ const contentSecurityPolicies = {
       ],
       'form-action': none,
       'frame-ancestors': none,
-      'img-src': 'http://odk-nginx.example.test/favicon.ico', // http:// allows secure upgrade to https://
+      'img-src': 'http://odk-nginx.example.test/favicon.ico', // http: scheme allows secure upgrade to https://
       'report-uri':  '/csp-report',
     },
   },
@@ -78,7 +78,7 @@ const contentSecurityPolicies = {
       ],
       'form-action': none,
       'frame-ancestors': self,
-      'img-src': 'http://odk-nginx.example.test/favicon.ico', // http:// allows secure upgrade to https://
+      'img-src': 'http://odk-nginx.example.test/favicon.ico', // http: scheme allows secure upgrade to https://
       'report-uri':  '/csp-report',
     }),
   },
