25351b12ff8ad568c554bdb6a75d14390a94af3c
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-04-28 14:21:41 +0300

    Apply suggestion from @alxndrsn

--- test/nginx/src/mocha/nginx.spec.js
+++ test/nginx/src/mocha/nginx.spec.js
@@ -54,7 +54,7 @@ const contentSecurityPolicies = {
       ],
       'form-action': none,
       'frame-ancestors': none,
-      'img-src': 'http://odk-nginx.example.test/favicon.ico', // http: scheme allows secure upgrade to https://
+      'img-src': 'http://odk-nginx.example.test/favicon.ico', // http: scheme permits secure upgrade to https://
       'report-uri':  '/csp-report',
     },
   },
