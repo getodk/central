376487cb31d01c69ea18d79404ae2ab656fde465
alxndrsn <alxndrsn>
2026-05-19 11:59:39 +0000

    revert eslintrc change

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'no-undef-init': 'error',
     'no-unused-expressions': 'error',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_$' }],
     'semi': [ 'error', 'always' ],
   },
 };
