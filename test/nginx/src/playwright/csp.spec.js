145a0789054bbb48f0f87ea62cb5da6227cedf0b
alxndrsn <alxndrsn>
2026-05-23 11:23:16 +0000

    update playwright

--- test/nginx/src/playwright/csp.spec.js
+++ test/nginx/src/playwright/csp.spec.js
@@ -37,7 +37,7 @@ test('catches style-src-elem violation samples', async ({ page }) => {
           'violated-directive': 'style-src-elem',
           'effective-directive': 'style-src-elem',
           'original-policy': `default-src 'report-sample' 'none'; connect-src 'self' https://translate.google.com https://translate.googleapis.com; font-src 'self'; form-action 'self'; frame-ancestors 'none'; frame-src 'self' https://getodk.github.io/central/; img-src data: https:; manifest-src 'self'; media-src 'none'; object-src 'none'; script-src 'report-sample' 'self'; style-src 'report-sample' 'self'; style-src-attr 'unsafe-inline'; worker-src 'report-sample' blob:; report-uri /csp-report`,
-          'disposition': 'report',
+          'disposition': 'enforce',
           'blocked-uri': 'inline',
           'line-number': 5,
           'column-number': 19,
