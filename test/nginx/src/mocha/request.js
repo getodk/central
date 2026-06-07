93a4f7d4e580db7214037a594825500a6a69fb99
alxndrsn <alxndrsn>
2026-06-07 11:43:11 +0000

    document better

--- test/nginx/src/mocha/request.js
+++ test/nginx/src/mocha/request.js
@@ -7,20 +7,13 @@ module.exports = request;
 //
 // 1. do not follow redirects
 // 2. allow overriding of fetch's "forbidden" headers: https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name
-function request(urlString, { body, ...options }={}) {
+function request(url, { body, ...options }={}) {
   if(!options.headers) options.headers = {};
   if(!options.headers.host) options.headers.host = 'odk-nginx.example.test';
 
-  const url = new URL(urlString);
-  if(url.username || url.password) throw new Error('Basic auth creds not yet supported.');
-
-  options.host = safeIpv6(url.hostname);
-  options.port = url.port;
-  options.path = urlString.replace(/^http(s?):\/\/[^/]*/, '') || '/';
-
   return new Promise((resolve, reject) => {
     try {
-      const req = getProtocolImplFrom(url).request(options, res => {
+      const req = getProtocolImplFrom(url).request({ ...options, ...preserve(url) }, res => {
         res.on('error', reject);
 
         const body = new Readable({ read:() => {} });
@@ -56,12 +49,30 @@ function request(urlString, { body, ...options }={}) {
   });
 }
 
-function getProtocolImplFrom({ protocol }) {
+function getProtocolImplFrom(url) {
+  const { protocol } = new URL(url);
   switch(protocol) {
     case 'http:':  return require('node:http');
     case 'https:': return require('node:https');
     default: throw new Error(`Unsupported protocol: ${protocol}`);
   }
+}
+
+/**
+ * Prevent URL path normalisation.
+ *
+ * @see https://nodejs.org/api/http.html#httprequesturl-options-callback
+ * @see https://nodejs.org/api/url.html#new-urlinput-base
+ */
+function preserve(urlString) {
+  const url = new URL(urlString);
+  if(url.username || url.password) throw new Error('Basic auth creds not yet supported.');
+
+  const host = safeIpv6(url.hostname);
+  const port = url.port;
+  const path = urlString.replace(/^http(s?):\/\/[^/]*/, '') || '/';
+
+  return { host, port, path };
 }
 
 function safeIpv6(hostname) {
