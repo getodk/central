3562b297e22399bb86146a56e4c743f7d27e8a56
alxndrsn <alxndrsn>
2026-06-16 10:48:12 +0000

    write client version in backward-compatible format

--- files/prebuild/write-version.sh
+++ files/prebuild/write-version.sh
@@ -5,6 +5,7 @@ shopt -s inherit_errexit
 {
   echo "versions:"
   echo "$(git rev-parse HEAD) ($(git describe --tags --always))"
+  echo " 0000000000000000000000000000000000000000 client ($FRONTEND_VERSION)"
   git submodule foreach --quiet --recursive \
     "commit=\$(git rev-parse HEAD); \
      tag=\$(git describe --tags --always); \
