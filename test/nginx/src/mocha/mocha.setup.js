cc5141088084105220109fe5229274327087dadf
alxndrsn <alxndrsn>
2026-07-06 09:56:34 +0000

    add helpful logging to document what this is

--- test/nginx/src/mocha/mocha.setup.js
+++ test/nginx/src/mocha/mocha.setup.js
@@ -1,9 +1,14 @@
 import http from 'node:http';
 import https from 'node:https';
 
+const log = (...args) => console.log('[mocha-setup]', ...args);
+
 export const mochaHooks = {
   afterAll() {
+    log('Cleaning up HTTP(S) Response objects whose bodies have not been read...');
     http.globalAgent.destroy();
     https.globalAgent.destroy();
+
+    log('Cleanup complete.');
   },
 };
