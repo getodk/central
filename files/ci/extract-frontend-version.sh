7e54400cf92f5621e437b88311cc381243999e40
alxndrsn <alxndrsn>
2026-07-07 03:28:26 +0000

    updated script

--- files/ci/extract-frontend-version.sh
+++ files/ci/extract-frontend-version.sh
@@ -2,13 +2,18 @@
 set -o pipefail
 shopt -s inherit_errexit
 
-log() { echo >&2 "[$(basname "$0")] $*"; }
+log() { echo >&2 "[$(basename "$0")] $*"; }
 
-log "Making sure .env exists..."
+log "Ensuring .env exists for docker compose..."
 touch .env
 
 log "Reading FRONTEND_VERSION from docker-compose.yml..."
-frontendVersion="$(docker compose config --format json | jq -er .services.nginx.build.args.FRONTEND_VERSION_DELIBERATELY_INCORRECT)"
+if ! frontendVersion="$(docker compose config --format json | jq -er .services.nginx.build.args.FRONTEND_VERSION_DELIBERATELY_INCORRECT)"; then
+	log "!!!"
+	log "!!! Failed to read FRONTEND_VERSION from docker-compose.yml (got: '$frontendVersion')."
+	log "!!!"
+  exit 1
+fi
 
 log "Writing FRONTEND_VERSION to GITHUB_ENV file ($GITHUB_ENV)..."
 echo "FRONTEND_VERSION=$FRONTEND_VERSION" >> "$GITHUB_ENV"
