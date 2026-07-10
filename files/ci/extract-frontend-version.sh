fa9bc2615498bf7044edac01d26cf70366badb4c
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-07-10 09:20:15 +0000

    ci: error if `FRONTEND_VERSION` not found (#2050)
    Re https://github.com/getodk/central/pull/2042#discussion_r3530680355
    
    Adding `-e` isn't enough to cause a helpful failure when `FRONTEND_VERSION` is missing from `docker-compose.yml` because:
    
    1. it doesn't fail, despite `bash -e`, because the `jq` command runs inside the `echo` call, which succeeds, and
    2. if the intention is for the failure to be easy to understand, `jq -e` won't help on its own

---
+++ files/ci/extract-frontend-version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+set -o pipefail
+shopt -s inherit_errexit
+
+log() { echo >&2 "[$(basename "$0")] $*"; }
+
+log "Ensuring .env exists for docker compose..."
+touch .env
+
+log "Reading FRONTEND_VERSION from docker-compose.yml..."
+if ! frontendVersion="$(docker compose config --format json | jq -er .services.nginx.build.args.FRONTEND_VERSION)"; then
+  log "!!!"
+  log "!!! Failed to read FRONTEND_VERSION from docker-compose.yml (got: '$frontendVersion')."
+  log "!!!"
+  exit 1
+fi
+
+log "Writing FRONTEND_VERSION to GITHUB_ENV file ($GITHUB_ENV)..."
+echo "FRONTEND_VERSION=$frontendVersion" >> "$GITHUB_ENV"
+
+log "Completed OK."
