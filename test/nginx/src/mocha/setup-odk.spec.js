b519034880ab820a6094a1a19bc10c7c28e21846
alxndrsn <alxndrsn>
2026-07-27 12:58:29 +0000

    handle wrong formats

--- test/nginx/src/mocha/setup-odk.spec.js
+++ test/nginx/src/mocha/setup-odk.spec.js
@@ -7,105 +7,56 @@ const log = (...args) => console.log('[setup-odk.spec]', ...args);
 const service = 'nginx-test-setup-odk';
 
 describe('setup-odk.sh', function() {
-  this.timeout(10_000);
+  describe('SENTRY_DSN_FRONTEND', () => {
+    afterEach(() => {
+      log('--- CONTAINER LOGS ---');
+      dockerCompose({}, `logs --timestamps ${service}`);
+      log('--- END CONTAINER LOGS ---');
+    });
+    after(() => {
+      dockerCompose({}, `down --remove-orphans --volumes`);
+    });
 
-  afterEach(() => {
-    log('--- CONTAINER LOGS ---');
-    dockerCompose({}, `logs --timestamps ${service}`);
-    log('--- END CONTAINER LOGS ---');
+    [
+      [ undefined, 'https://o-fake-dsn.ingest.sentry.io' ],
+      [
+        'https://abcdef0123456789abcdef0123456789@some-dsn.ingest.sentry.io/1234567890123456',
+        'https://some-dsn.ingest.sentry.io',
+      ],
+      [ '', '' ],
+      [ 'bad-format', '' ],
+    ].forEach(([ SENTRY_DSN_FRONTEND, expectedCspEntry ]) => {
+      it(`should generated expected CSP for SENTRY_DSN_FRONTEND='${SENTRY_DSN_FRONTEND}'`, withNginx({
+        SENTRY_DSN_FRONTEND,
+      }, async () => {
+        // when
+        const res = await request(`https://localhost:10003`);
+
+        // then
+        assert.equal(res.status, 200);
+        assert.equal(
+          res.headers.get('Content-Security-Policy'),
+          [
+            `default-src 'report-sample' 'none'`,
+            `connect-src 'self' ${expectedCspEntry} https://translate.google.com https://translate.googleapis.com`,
+            `font-src 'self'`,
+            `form-action 'self'`,
+            `frame-ancestors 'none'`,
+            `frame-src 'self' https://getodk.github.io/central/`,
+            `img-src data: https:`,
+            `manifest-src 'self'`,
+            `media-src 'none'`,
+            `object-src 'none'`,
+            `script-src 'report-sample' 'self'`,
+            `style-src 'report-sample' 'self'`,
+            `style-src-attr 'unsafe-inline'`,
+            `worker-src 'report-sample' blob:`,
+            `report-uri /csp-report`,
+          ].join('; '),
+        );
+      }));
+    });
   });
-  after(() => {
-    dockerCompose({}, `down --remove-orphans --volumes`);
-  });
-
-  it('should start ok with basic config', withNginx({
-  }, async () => {
-    // when
-    const res = await request(`https://localhost:10003`);
-
-    // then
-    assert.equal(res.status, 200);
-    assert.equal(
-      res.headers.get('Content-Security-Policy'),
-      [
-        `default-src 'report-sample' 'none'`,
-        `connect-src 'self' https://o-fake-dsn.ingest.sentry.io https://translate.google.com https://translate.googleapis.com`,
-        `font-src 'self'`,
-        `form-action 'self'`,
-        `frame-ancestors 'none'`,
-        `frame-src 'self' https://getodk.github.io/central/`,
-        `img-src data: https:`,
-        `manifest-src 'self'`,
-        `media-src 'none'`,
-        `object-src 'none'`,
-        `script-src 'report-sample' 'self'`,
-        `style-src 'report-sample' 'self'`,
-        `style-src-attr 'unsafe-inline'`,
-        `worker-src 'report-sample' blob:`,
-        `report-uri /csp-report`,
-      ].join('; '),
-    );
-  }));
-
-  it('should serve a coherent config with SENTRY_DSN_FRONTEND set', withNginx({
-    SENTRY_DSN_FRONTEND: 'https://abcdef0123456789abcdef0123456789@some-dsn.ingest.sentry.io/1234567890123456',
-  }, async () => {
-    // when
-    const res = await request(`https://localhost:10003`);
-
-    // then
-    assert.equal(res.status, 200);
-    assert.equal(
-      res.headers.get('Content-Security-Policy'),
-      [
-        `default-src 'report-sample' 'none'`,
-        `connect-src 'self' https://some-dsn.ingest.sentry.io https://translate.google.com https://translate.googleapis.com`,
-        `font-src 'self'`,
-        `form-action 'self'`,
-        `frame-ancestors 'none'`,
-        `frame-src 'self' https://getodk.github.io/central/`,
-        `img-src data: https:`,
-        `manifest-src 'self'`,
-        `media-src 'none'`,
-        `object-src 'none'`,
-        `script-src 'report-sample' 'self'`,
-        `style-src 'report-sample' 'self'`,
-        `style-src-attr 'unsafe-inline'`,
-        `worker-src 'report-sample' blob:`,
-        `report-uri /csp-report`,
-      ].join('; '),
-    );
-  }));
-
-  it('should serve a coherent config with SENTRY_DSN_FRONTEND blank', withNginx({
-    SENTRY_DSN_FRONTEND: '',
-  }, async () => {
-    // when
-    const res = await request(`https://localhost:10003`);
-
-    // then
-    assert.equal(res.status, 200);
-    assert.equal(
-      res.headers.get('Content-Security-Policy'),
-      [
-        `default-src 'report-sample' 'none'`,
-        `connect-src 'self'  https://translate.google.com https://translate.googleapis.com`,
-        `font-src 'self'`,
-        `form-action 'self'`,
-        `frame-ancestors 'none'`,
-        `frame-src 'self' https://getodk.github.io/central/`,
-        `img-src data: https:`,
-        `manifest-src 'self'`,
-        `media-src 'none'`,
-        `object-src 'none'`,
-        `script-src 'report-sample' 'self'`,
-        `style-src 'report-sample' 'self'`,
-        `style-src-attr 'unsafe-inline'`,
-        `worker-src 'report-sample' blob:`,
-        `report-uri /csp-report`,
-      ].join('; '),
-    );
-  }));
 });
 
 function dockerCompose(opts, ...args) {
@@ -116,7 +67,9 @@ function dockerCompose(opts, ...args) {
 }
 
 function withNginx(env, fn) {
-  return async () => {
+  return async function() {
+    this.timeout(10_000);
+
     dockerCompose({ env }, `up --build --force-recreate --detach --wait ${service}`);
 
     await fn();
