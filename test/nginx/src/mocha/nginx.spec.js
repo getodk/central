f01a8c71cd870d4db0c2f14cf8d41d2daf021ec5
alxndrsn <alxndrsn>
2026-08-28 07:51:39 +0000

    wip

--- test/nginx/src/mocha/nginx.spec.js
+++ test/nginx/src/mocha/nginx.spec.js
@@ -82,7 +82,7 @@ const contentSecurityPolicies = {
       ],
       'font-src':       self,
       'form-action': self,
-      'frame-ancestors': self,
+      'frame-ancestors': none,
       'frame-src':      [
         self,
         centralNotifications,
