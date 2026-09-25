9a9f6a887fd7df8cdedb47fced50750686f70946
alxndrsn <alxndrsn>
2026-09-25 06:49:05 +0000

    test/with-pgenvblock: add additional test cases

--- test/test-with-pgenvblock.sh
+++ test/test-with-pgenvblock.sh
@@ -4,25 +4,41 @@ shopt -s inherit_errexit
 
 log() { echo >&2 "[test-with-pgenvblock] $*"; }
 
+test_env() {
+  title="$1"
+  envblock="$2"
+  expectedEnv="$3"
+
+  log "  Testing: $title..."
+  if diff \
+      <(xxd <(
+        env --ignore-environment \
+            files/service/with-pgenvblock.pl \
+            <(printf "$envblock") \
+            env --null
+      )) \
+      <(xxd <(printf "$expectedEnv"))
+  then
+    log "    Passed OK."
+  else
+    log "!!!"
+    log "!!! Test failed; see above for differences between '< actual' and '> expected'"
+    log "!!!"
+    exit 1
+  fi
+}
+
 log "Testing..."
 
-log "  Testing: specific postgres-related variables..."
-if diff \
-    <(xxd <(
-      env --ignore-environment \
-          files/service/with-pgenvblock.pl \
-          <(printf "A=1\0PGSSLMODE=2\0C=3\0PGDATABASE=4\0E=5\0NODE_EXTRA_CA_CERTS=5\0G=7") \
-          env --null
-    )) \
-    <(xxd <(printf "PGSSLMODE=2\0PGDATABASE=4\0"))
-then
-  log "    Passed OK."
-else
-  log "!!!"
-  log "!!! Test failed; see above for differences between '< actual' and '> expected'"
-  log "!!!"
-  exit 1
-fi
+test_env "empty env" "" ""
+
+test_env "no postgres-related vars 1" "A=1" ""
+test_env "no postgres-related vars 2" "A=1\0B=2" ""
+test_env "no postgres-related vars 3" "A=1\0B=2\0C=3" ""
+
+test_env "specific postgres-related variables" \
+         "A=1\0PGSSLMODE=2\0C=3\0PGDATABASE=4\0E=5\0NODE_EXTRA_CA_CERTS=5\0G=7" \
+         "PGSSLMODE=2\0PGDATABASE=4\0"
 
 log "  Testing: local env + an injected var..."
 # for the skeptical reader, on the below:
